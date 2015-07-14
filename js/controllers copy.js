e1a4fb44a0a1d7e2a36b2c548cf9a42776f772d7
Michael Cooper <mikejcooper1@gmail.com>
2015-07-14 13:26:50 +0100

    fix

--- js/controllers copy.js
+++ js/controllers copy.js
@@ -76,45 +76,38 @@ angular.module('starter.controllers',['ui.bootstrap'])
   ];
       //Device selection for Maps integration.
       $scope.changeLinkMaps = function (location){
-        //var devicePlatform = device.platform;
+        var devicePlatform = device.platform;
+        var mapRoot = location + ", UK";
 
-          var mapRoot = location + ", UK";
-
-        //console.log(devicePlatform);
-        //if(devicePlatform == "iOS"){
-        //  mapRoot = "maps:q=" + mapRoot;
-        //  document.getElementById("maps").href = mapRoot
-        //  document.getElementById("maps2").href = mapRoot
-        //}
-        //else if (devicePlatform == 'Android'){
-        //  mapRoot = "geo:0,0?q=" + mapRoot
-        //  document.getElementById("maps").href = mapRoot
-        //  document.getElementById("maps2").href = mapRoot
-        //}
-        //else {
-        //  mapRoot = window.open("https://www.google.co.uk/maps/search/" + mapRoot, '_system', $location = 'no');
-        //}
-
-          var confirmPopup = $ionicPopup.confirm({
+        var confirmPopup = $ionicPopup.confirm({
               title: 'Open in browser',
               cancelText: 'Cancel',
               okText: 'Ok'
-          });
-          //
-          confirmPopup.then(function (res) {
-              if (res) {
-                  console.log('You are sure');
-                  navigator.app.loadUrl('https://google.com/', { openExternal:true });
-              } else {
-                  console.log('You are not sure');
+        });
+        confirmPopup.then(function (res) {
+            if (res) {
+                console.log('You are sure');
+                if(devicePlatform == "iOS"){
+                    mapRoot = "http://maps.apple.com/?q=" + mapRoot;
+                }
+                else if (devicePlatform == 'Android'){
+                    mapRoot = "geo:" + mapRoot;
+                }
+                else {
+                    mapRoot = "https://www.google.co.uk/maps/search/" + mapRoot;
+                }
+                openLink(mapRoot);
+                console.log('HERE');
+              }
+              else {
+                console.log('You are not sure');
               }
           });
-
           return false;
       };
 
       //Add to config.xml for appAvailability plugin ->>> <gap:plugin name="com.ohh2ahh.plugins.appavailability" />
-      $scope.changeLinkImages = function(URL, id){
+      $scope.changeLinkImages = function(URL){
           console.log("here");
         //var scheme  = 'fb://';
         //appAvailability.check(scheme,       // URI Scheme or Package Name
@@ -135,8 +128,7 @@ angular.module('starter.controllers',['ui.bootstrap'])
           confirmPopup.then(function (res) {
               if (res) {
                   console.log('You are sure');
-                  navigator.app.loadUrl('https://google.com/', { openExternal:true });
-                  //window.open(URL, '_system', $location = 'no');
+                  openLink(URL);
               } else {
                   console.log('You are not sure');
               }
@@ -165,13 +157,11 @@ angular.module('starter.controllers',['ui.bootstrap'])
             confirmPopup.then(function (res) {
                 if (res) {
                     console.log('You are sure');
-                    window.open("tel:"+ phoneNumber, '_system', $location = 'no');
+                    openLink("tel:"+ phoneNumber);
                 } else {
                     console.log('You are not sure');
                 }
             });
-
-
 
 
             $scope.makeCall = function () {
@@ -193,5 +183,23 @@ angular.module('starter.controllers',['ui.bootstrap'])
         }
 
 });
+
+var openLink = function (URL){
+
+    var devicePlatform = device.platform;
+
+    if(devicePlatform == "iOS"){
+        window.open(URL, '_system', $location = 'no');
+    }
+    else if (devicePlatform == 'Android'){
+        navigator.app.loadUrl(URL, { openExternal:true });
+    }
+    else {
+        window.open(URL, '_system', $location = 'no');
+    }
+    console.log('LINK opened');
+
+    return false;
+};
 
 
